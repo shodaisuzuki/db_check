4055a4eff789c72de0d28f66c1fa4f2d8c381f9c
shodaisuzuki <shodai-connect@tbz.t-com.ne.jp>
2017-10-10 15:36:07 +0900

    create_csv.sh追加
    指定したテーブルをCSV出力する

---
+++ create_csv.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+SOURCE_DB_NAME=""
+SOURCE_USER_NAME=""
+SOURCE_HOST_NAME=""
+SOURCE_PASSWORD=""
+SOURCE_PORT=""
+SOURCE_OPTIONS="-U $SOURCE_USER_NAME -h $SOURCE_HOST_NAME -p $SOURCE_PORT -d $SOURCE_DB_NAME"
+
+# CSV出力するテーブルを指定
+TABLES=( 
+ ""
+)
+
+# 各テーブルごとにCSV出力
+for table in "${TABLES[@]}"
+do
+    export PGPASSWORD=$SOURCE_PASSWORD
+    
+    COLUMN_LIST_SQL="SELECT column_name FROM information_schema.columns where table_name = '\''$table'\'';"
+    COLUMN_LIST_CMD="echo '${COLUMN_LIST_SQL}' | psql $SOURCE_OPTIONS -t "
+    COLUMN_LIST=(`eval $COLUMN_LIST_CMD`)
+    COLUMN="$(IFS=,; echo "${COLUMN_LIST[*]}")"
+    
+    OUTPUT_DIR=./csv 
+    
+    if [ ! -e $OUTPUT_DIR ]; then
+      mkdir $OUTPUT_DIR
+    fi
+
+   #CSV出力
+    SOURCE_SQL="select ${COLUMN} FROM $table"
+    SOURCE_CMD="psql -U ${SOURCE_USER_NAME} ${SOURCE_DB_NAME} -c '${SOURCE_SQL}' -A -F, > csv/${table}.csv"    
+    eval $SOURCE_CMD
+    echo $table
+done
+
+echo "DONE!!"
