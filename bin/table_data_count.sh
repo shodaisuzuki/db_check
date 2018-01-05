407f7ec09c5798e164ca21b62ac820744c61cf83
shodaisuzuki <shodai-connect@tbz.t-com.ne.jp>
2018-01-05 10:58:32 +0900

    "table_data_count.sh"をpsql.conf対応

--- bin/table_data_count.sh
+++ bin/table_data_count.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-PG_PASSWORD=""
-DB_NAME=""
-USER_NAME=""
-HOST_NAME=""
-PORT=""
-OPTIONS="-U $USER_NAME -h $HOST_NAME -p $PORT -d $DB_NAME"
-EXPORT_FILE_NAME="table_data_count.csv"
- 
+source ../psql.conf
+EXPORT_FILE_NAME="table_data_count"
+
 export PGPASSWORD=$PG_PASSWORD
 指定したデーベースのテーブル名を全て取得
 TABLE_LIST_CMD="echo 'select relname as TABLE_NAME from pg_stat_user_tables;' | psql $OPTIONS -t"
@@ -18,5 +13,5 @@ for table in "${TABLES[@]}"
 do
   COUNT_SQL="select count(*) FROM $table"  COUNT_CMD="echo '${COUNT_SQL};' | psql ${OPTIONS} | head -3 | tail -n 1"
   COUNT_RESULT=(`eval $COUNT_CMD`)
-  echo "$table,$COUNT_RESULT" >> EXPORT_FILE_NAME
+  echo "$table,$COUNT_RESULT" >> "$EXPORT_FILE_NAME-$TIMESTAMP.csv"
 done
