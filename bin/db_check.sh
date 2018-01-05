a7fadf8ba190178fed9b62d53143bc8b2bc4749c
shodaisuzuki <shodai-connect@tbz.t-com.ne.jp>
2018-01-05 11:10:54 +0900

    "db_check.sh"をpsql.conf対応

--- bin/db_check.sh
+++ bin/db_check.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-SOURCE_PASSWORD=""
+
+source ../psql.conf
 SOURCE_DB_NAME=""
 SOURCE_USER_NAME=""
 SOURCE_HOST_NAME=""
 SOURCE_PASSWORD=""
 SOURCE_PORT=""
-SOURCE_OPTIONS="-U $SOURCE_USER_NAME -h $SOURCE_HOST_NAME -p $SOURCE_PORT -d $SOURCE_DB_NAME"
 
-TARGET_PASSWORD=""
 TARGET_DB_NAME=""
 TARGET_USER_NAME=""
 TARGET_HOST_NAME=""
 TARGET_PASSWORD=""
 TARGET_PORT=""
-TARGET_OPTIONS="-U $TARGET_USER_NAME -h $TARGET_HOST_NAME -p $TARGET_PORT -d $TARGET_DB_NAME"
 
 # 指定したデーベースのテーブル名を全て取得
-CMD="echo 'select relname as TABLE_NAME from pg_stat_user_tables;' | psql $TARGET_OPTIONS -t"
+CMD="echo 'select relname as TABLE_NAME from pg_stat_user_tables;' | psql $/OPTIONS -t"
 TABLES=(`eval $CMD`)
 
 # 各テーブルごとにcountの結果を比較
@@ -25,12 +23,12 @@ for table in "${TABLES[@]}"
 do
     export PGPASSWORD=$SOURCE_PASSWORD
     SOURCE_SQL="select count(*) FROM $table;"
-    SOURCE_CMD="echo '${SOURCE_SQL};' | psql ${SOURCE_OPTIONS} | head -3 | tail -n 1"
+    SOURCE_CMD="echo '${SOURCE_SQL};' | psql ${OPTIONS} | head -3 | tail -n 1"
     SOURCE_TABLECOUNT=(`eval $SOURCE_CMD`)
 
     export PGPASSWORD=$TARGET_PASSWORD
     TARGET_SQL="select count(*) FROM $table"
-    TARGET_CMD="echo '${TARGET_SQL};' | psql ${TARGET_OPTIONS} | head -3 | tail -n 1"
+    TARGET_CMD="echo '${TARGET_SQL};' | psql ${OPTIONS} | head -3 | tail -n 1"
     TARGET_TABLECOUNT=(`eval $TARGET_CMD`)
 
     if [ $SOURCE_TABLECOUNT = $TARGET_TABLECOUNT ]; then
