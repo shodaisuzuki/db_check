639fc0ceac17a552a3dd660a853cf1313681cf4f
shodaisuzuki <shodai-connect@tbz.t-com.ne.jp>
2018-01-16 10:52:56 +0900

    出力ファイル名を変数化

--- bin/table_data_count.sh
+++ bin/table_data_count.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 source ../psql.conf
-EXPORT_FILE_NAME="table_data_count"
+EXPORT_FILE_NAME="table_data_count_$TIMESTAMP"
 
 export PGPASSWORD=$PG_PASSWORD
-指定したデーベースのテーブル名を全て取得
-TABLE_LIST_CMD="echo 'select relname as TABLE_NAME from pg_stat_user_tables;' | psql $OPTIONS -t"
+psql.confで指定したデータベースのテーブル名を全て取得
+TABLE_LIST_CMD="echo 'select relnameas TABLE_NAME from pg_stat_user_tables;' | psql $OPTIONS -t"
 TABLES=(`eval $TABLE_LIST_CMD`)
 
 # 各テーブルごとにテーブル名,count結果をCSV出力する
@@ -13,5 +13,7 @@ for table in "${TABLES[@]}"
 do
   COUNT_SQL="select count(*) FROM $table"  COUNT_CMD="echo '${COUNT_SQL};' | psql ${OPTIONS} | head -3 | tail -n 1"
   COUNT_RESULT=(`eval $COUNT_CMD`)
-  echo "$table,$COUNT_RESULT" >> "../$EXPORT_DIR/$EXPORT_FILE_NAME-$TIMESTAMP.csv"
+  echo "$table,$COUNT_RESULT" >> "../$EXPORT_DIR/$EXPORT_FILE_NAME.csv"
 done
+
+echo "DONE! open to $EXPORT_DIR/$EXPORT_FILE_NAME.csv"
