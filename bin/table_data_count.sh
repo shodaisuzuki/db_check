defbc444c587c0ad91ef76f9da6d6d4119c57007
shodaisuzuki <shodai-connect@tbz.t-com.ne.jp>
2018-01-16 10:57:04 +0900

    出力するcsvにヘッダを挿入

--- bin/table_data_count.sh
+++ bin/table_data_count.sh
@@ -8,6 +8,9 @@ psql.confで指定したデータベースのテーブル名を全て取得
 TABLE_LIST_CMD="echo 'select relnameas TABLE_NAME from pg_stat_user_tables;' | psql $OPTIONS -t"
 TABLES=(`eval $TABLE_LIST_CMD`)
 
+#出力csvのヘッダを挿入
+echo "table_name,count" >> "../$EXPORT_DIR/$EXPORT_FILE_NAME.csv"
+
 # 各テーブルごとにテーブル名,count結果をCSV出力する
 for table in "${TABLES[@]}"
 do
